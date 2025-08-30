7d15133380a8bf4aa45f62ff23493b5911d43c93
PetchAuisui <petchauisui50@gmail.com>
2025-08-30 22:33:38 +0700

    updata main.py , create /register

--- main.py
+++ main.py
@@ -7,6 +7,20 @@ app = Flask(__name__)
 def home():
     return "Hello, Flask!"
 
+from flask import Flask, request, jsonify
+from services.user_service import UserService
+
+app = Flask(__name__)
+
+@app.route("/register", methods=["POST"])
+def register():
+    data = request.get_json()
+    response, status = UserService.register(data)
+    return jsonify(response), status
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
