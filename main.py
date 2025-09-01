898ab6b49ceb6c5809b5d502aa92e84519c41bb9
Siwapat <petchauisui50Ggmail.com>
2025-09-01 19:58:14 +0700

    update main.py

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ from services.user_service import UserService
 
 app = Flask(__name__)
 
+user_service = UserService(JsonUserRepository())
+
 @app.route("/register", methods=["POST"])
 def register():
     data = request.get_json()
-    response, status = UserService.register(data)
+    response, status = user_service.register(data)
     return jsonify(response), status
+
+@app.route("/users", methods=["GET"])
+def get_users():
+    return jsonify(user_service.get_all_users()), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
