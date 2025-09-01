94643d22b11c901e4698af88d7b770d53bc81c30
Siwapat <petchauisui50Ggmail.com>
2025-09-01 19:51:11 +0700

    remodel

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-# สร้าง route ทดสอบ
-@app.route("/")
-def home():
-    return "Hello, Flask!"
-
 from flask import Flask, request, jsonify
 from services.user_service import UserService
 
@@ -17,10 +8,6 @@ def register():
     data = request.get_json()
     response, status = UserService.register(data)
     return jsonify(response), status
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
