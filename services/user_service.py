fbafe08da26a45265cb45b0c9d268dd148165f10
Siwapat <petchauisui50Ggmail.com>
2025-09-01 19:57:42 +0700

    update user_service.py

--- services/user_service.py
+++ services/user_service.py
@@ -1,11 +1,11 @@
 from models.user import User
 
-# Low Coupling: list เก็บ user อยู่ภายใน service
-users = []
-
 class UserService:
-    @staticmethod
-    def validate_data(data):
+    def __init__(self, repository):
+        self.repository = repository
+        self.users = self.repository.load_users()
+
+    def validate_data(self, data):
         required_fields = ["name","id_card","major","member_type","phone","email","address","password","confirm_password"]
         for field in required_fields:
             if field not in data:
@@ -17,16 +17,16 @@ class UserService:
         if not data["email"].endswith("@kmitl.ac.th"):
             return "Email must end with @kmitl.ac.th"
 
-        for u in users:
+        for u in self.users:
             if u.email == data["email"]:
                 return "Email already registered"
             if u.id_card == data["id_card"]:
                 return "ID card already registered"
+
         return None
 
-    @staticmethod
-    def register(data):
-        error = UserService.validate_data(data)
+    def register(self, data):
+        error = self.validate_data(data)
         if error:
             return {"error": error}, 400
 
@@ -40,5 +40,9 @@ class UserService:
             address=data["address"],
             password=data["password"]
         )
-        users.append(user)
+        self.users.append(user)
+        self.repository.save_users(self.users)
         return {"message": "Register successful", "user": user.to_dict()}, 201
+
+    def get_all_users(self):
+        return [u.to_dict() for u in self.users]
