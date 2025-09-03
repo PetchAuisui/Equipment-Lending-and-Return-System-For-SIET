736eb52745820d7676823fda525a78d88b60e366
thepthut <67030296@kmitl.ac.th>
2025-09-03 20:17:25 +0700

    updata main

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def home():
     return "Hello, Flask!"
 
+@app.route("/delay")
+def delayed_notification():
+    return "ครบกำหนดเล้ว เอาของมาคืนด้วยนะครับ"
 
 if __name__ == "__main__":
     app.run(debug=True)
