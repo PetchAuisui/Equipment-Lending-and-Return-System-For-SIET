08187537dca2dd356f04b007822905d11d252e0a
Paphatsorn <paphattssorn@gmail.com>
2025-09-03 18:51:58 +0700

    first commit

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 # สร้าง route ทดสอบ
-@app.route("/")
+@app.route("/device")
 def home():
-    return "Hello, Flask!"
-
+    return "ทดสอบ 1 2 3!"
 
 if __name__ == "__main__":
     app.run(debug=True)
