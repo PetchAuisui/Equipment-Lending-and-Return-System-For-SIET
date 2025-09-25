9ccde0263ff54dfb9dc175f81ce3589b07a348f2
Ice-Ittikorn <67030261@kmitl.ac.th>
2025-09-25 17:06:31 +0700

    create lend.html

--- app/blueprints/inventory/routes.py
+++ app/blueprints/inventory/routes.py
@@ -3,4 +3,9 @@ from . import inventory_bp
 
 @inventory_bp.get("/")
 def equipment_index():
+    return render_template("pages_inventory/lend_device.html")
+
+@inventory_bp.get("/lend")
+def lend():
     return render_template("pages_inventory/lend.html")
+
