af68557c28ef20aa115b5a6472a553764d5c6c1c
Ice-Ittikorn <67030261@kmitl.ac.th>
2025-09-26 18:39:57 +0700

    merge feature/lend_device to develop

--- app/blueprints/inventory/routes.py
+++ app/blueprints/inventory/routes.py
@@ -1,6 +1,16 @@
 from flask import render_template
-from . import inventory_bp
+from app.blueprints.inventory import inventory_bp
+from app.services import lend_device_service
 
-@inventory_bp.get("/")
-def equipment_index():
-    return render_template("inventory/index.html")
+@inventory_bp.route('/lend_device')
+def lend_device():
+    # 📌 ดึงข้อมูลจาก service
+    equipments = lend_device_service.get_equipment_list()
+    
+    # ✅ return template พร้อมส่งข้อมูลไปยัง HTML
+    return render_template("pages_inventory/lend_device.html", equipments=equipments)
+
+
+@inventory_bp.route('/lend')
+def lend():
+    return render_template("pages_inventory/lend.html")
