be865b80374133ae63dad3717bb4f1276bad1e39
Ice-Ittikorn <67030261@kmitl.ac.th>
2025-09-26 10:37:55 +0700

    update

--- app/blueprints/inventory/routes.py
+++ app/blueprints/inventory/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template
 from . import inventory_bp
 
-@inventory_bp.get("/")
+@inventory_bp.get("/lend_device")
 def equipment_index():
     return render_template("pages_inventory/lend_device.html")
 
