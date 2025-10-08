e1d9c0b20101d7dd3b538f1d758f230402ca057d
Ice-Ittikorn <67030261@kmitl.ac.th>
2025-10-08 21:32:50 +0700

    add time_lend name phone auto

--- app/blueprints/inventory/__init__.py
+++ app/blueprints/inventory/__init__.py
@@ -2,3 +2,11 @@ from flask import Blueprint
 inventory_bp = Blueprint("inventory", __name__)
 from . import routes  # noqa
 
+from datetime import datetime
+
+
+
+# ✅ ฟังก์ชันนี้จะทำให้ Jinja ใช้ now() ได้ในทุก template
+@inventory_bp.app_context_processor
+def inject_now():
+    return {'now': datetime.now}
