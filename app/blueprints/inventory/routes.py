1266ee157d80e1e3a3eba22fc6c0a661bf266614
Paphatsorn <paphattssorn@gmail.com>
2025-10-10 15:21:58 +0700

    update models/equipment

--- app/blueprints/inventory/routes.py
+++ app/blueprints/inventory/routes.py
@@ -2,8 +2,8 @@ from flask import render_template, request, redirect, url_for, current_app, flas
 from app.blueprints.inventory import inventory_bp
 from app.services.lend_device_service import get_grouped_equipments_separated
 from app.db.db import SessionLocal
-from app.models.equipment import Equipment
-from app.db.models import EquipmentImage
+from app.db.models import Equipment
+from app.db.models import Equipment, EquipmentImage
 from sqlalchemy.orm import joinedload
 from sqlalchemy.exc import IntegrityError
 from werkzeug.utils import secure_filename
