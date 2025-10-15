c72dec33cf89d593eac754adde163a35a1cde336
PetchAuisui <petchauisui50@gmail.com>
2025-10-16 01:31:56 +0700

    update connect-database

--- app/db/db.py
+++ app/db/db.py
@@ -1,17 +1,26 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker, scoped_session
 
-DATABASE_URL = "sqlite:///app.db"  # เก็บ DB ใน instance/
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///app.db")
+
+
+connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
 
 engine = create_engine(
     DATABASE_URL,
     future=True,
     echo=False,
-    connect_args={"check_same_thread": False},
+    connect_args=connect_args,
 )
 
 SessionLocal = scoped_session(
-    sessionmaker(bind=engine, autoflush=False, autocommit=False, expire_on_commit=False)
+    sessionmaker(
+        bind=engine,
+        autoflush=False,
+        autocommit=False,
+        expire_on_commit=False,
+    )
 )
 
 Base = declarative_base()
