37d849087e1558b7f80e16f4e9492d8391f59a98
PetchAuisui <petchauisui50@gmail.com>
2025-10-16 01:07:47 +0700

    update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from app import create_app
+import os
+
+app = create_app()
 
 if __name__ == "__main__":
-    import os
-    app = create_app()
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)), debug=os.getenv("FLASK_DEBUG","0")=="1")
+    app.run(
+        host="0.0.0.0",
+        port=int(os.getenv("PORT", 5000)),
+        debug=os.getenv("FLASK_DEBUG", "0") == "1",
+    )
